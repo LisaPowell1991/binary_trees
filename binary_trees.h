c614ac10a1f528403a886cb3c5b4b48a2220ff8a
LisaPowell1991 <lisapowell46@gmail.com>
2023-10-31 00:40:27 -0700

    updated the header file with all the prototypes that will be created

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,61 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
+/*new node*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/*Insert left*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/*Insert right*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/*Delete*/
+void binary_tree_delete(binary_tree_t *tree);
+
+/*Is leaf*/
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/*Is root*/
+int binary_tree_is_root(const binary_tree_t *node);
+
+/*Pre-order traversal*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/*In-order traversal*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/*Post-order traversal*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/*Height*/
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/*Depth*/
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/*Size*/
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/*Leaves*/
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/*Nodes*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/*Balance factor*/
+int binary_tree_balance(const binary_tree_t *tree);
+
+/*Is full*/
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/*Is perfect*/
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/*Sibling*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/*Uncle*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif /* _BINARY_TREES_H_ */
